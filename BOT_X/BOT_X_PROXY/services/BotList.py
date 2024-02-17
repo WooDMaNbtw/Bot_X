cdc55e3cd71a867f40fd2c203985cbb924b73f17
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-02-17 16:17:16 +0400

    init bot_x

---
+++ BOT_X/BOT_X_PROXY/services/BotList.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView
+from core.Route import Route
+
+
+class BotList(Route, APIView):
+
+    def get_method(self) -> str:
+        return "GET"
+
+    def get_path(self) -> str:
+        return "/bots"
---
+++ BOT_X/BOT_X_PROXY/services/EmailVerificationResend.py
@@ -0,0 +1,11 @@
+from core.Route import Route
+from rest_framework.views import APIView
+
+
+class EmailVerificationResend(Route, APIView):
+
+    def get_method(self) -> str:
+        return "POST"
+
+    def get_path(self) -> str:
+        return f"/users/email-verification/resend"
